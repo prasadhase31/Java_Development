e8d1c6c4e7dbbae5935a05f06010889cb060f201
prasadhase31 <prasadhase9325@gmail.com>
2026-01-09 23:52:13 +0530

    Pattern condition

---
+++ Pattern with codnition/p7.java
@@ -0,0 +1,27 @@
+/*
+55555
+54444
+54333
+54322
+54321
+*/
+
+class p7{
+	
+	public static void main(String[] args) {
+		
+		for(int i=5;i>=1;i--){
+			for(int j=5;j>=1;j--){
+
+				if(j>i){
+
+					System.out.print(j);
+				}else{
+
+					System.out.print(i);
+				}
+			}
+			System.out.println();
+		}
+	}
+}
