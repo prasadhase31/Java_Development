acf9fddf564810aad1957a142348e347430d967f
prasadhase31 <prasadhase9325@gmail.com>
2026-01-10 00:16:45 +0530

    Pattern condition

---
+++ Pattern with codnition/p9.java
@@ -0,0 +1,28 @@
+/*
+54321
+44321
+33321
+22221
+11111
+*/
+
+class p9{
+	
+	public static void main(String[] args) {
+		
+		for(int i=5;i>=1;i--){
+
+			for(int j=5;j>=1;j--){
+
+				if(j>i){
+
+					System.out.print(i);
+
+				}else{
+					System.out.print(j);
+				}
+			}
+			System.out.println();
+		}
+	}
+}
