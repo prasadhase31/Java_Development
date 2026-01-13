51bf4a66025eac2dddf0692267c12fe6c0e50ace
prasadhase31 <prasadhase9325@gmail.com>
2026-01-13 20:35:31 +0530

    numbers problem

--- Number & Digit/palindrome.java
+++ Number & Digit/palindrome.java
@@ -7,7 +7,7 @@ class palindrome{
 		Scanner sc=new Scanner(System.in);
 
 		System.out.println("Enter a number");
-		int num=sc.nextInt();
+		int num=sc.nextInt(); 
           
          int original=num;
          int reverse=0;
---
+++ Pattern with codnition/factor.java
@@ -0,0 +1,17 @@
+class factor{
+	
+	public static void main(String[] args) {
+		
+		Scanner sc =new Scanner(System.in);
+
+		System.out.println("enter a number");
+		int num=sc.nextInt();
+
+		int fact=1;
+
+		while(num !=0){
+
+			
+		}
+	}
+}
