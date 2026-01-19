8efd776c4c85a6dc1db66e6570653303bd0d43f4
prasadhase31 <prasadhase9325@gmail.com>
2026-01-20 01:19:52 +0530

    array

---
+++ Array/p1.j.java
@@ -0,0 +1,9 @@
+class p1{
+	
+	public static void main(String[] args) {
+		
+		sum=int arr[]=[5,8,12,6];
+
+		System.out.println(sum);
+	}
+}
---
+++ Array/p1.java
@@ -0,0 +1,15 @@
+class p1{
+	
+	public static void main(String[] args) {
+		
+		int arr[]={5,8,12,6};
+		int sum=0;
+
+		for(int i=0;i<arr.length;i++){
+
+			sum+=arr[i];
+		}
+
+		System.out.println("sum =" +sum);
+	}
+}
