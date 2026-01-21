d0438c18356e19df8f660620259c850857b93feb
prasadhase31 <prasadhase9325@gmail.com>
2026-01-21 23:51:57 +0530

    array

---
+++ Array/p3.java
@@ -0,0 +1,21 @@
+class p3{
+	
+	public static void main(String[] args) {
+		
+		int arr[]={11,22,33,44,55};
+		int sum=0;
+
+		System.out.println("odd elements=");
+
+		for(int i=0;i<arr.length;i++){
+          if(arr[i]%2!=0){
+          	System.out.println(arr[i]+" ");
+          	sum=sum+arr[i];
+          }
+          }
+          System.out.println();
+          System.out.println("sum ="+sum);
+
+
+		}
+	}
