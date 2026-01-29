4d6250c1b1c5cae51c350691c5ad59be56d783f5
prasadhase31 <prasadhase9325@gmail.com>
2026-01-29 13:46:12 +0530

    array

---
+++ Array/p5.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class P5 {
+    public static void main(String[] args) {
+
+        int arr[]=new int[n];
+
+        Scanner sc=new Scanner(System.in);
+
+        System.out.print("Enter array elements");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.print("Reversed array =");
+        for(int i=arr.length-1;i>=0;i--){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
---
+++ Array/p6.java
@@ -0,0 +1,31 @@
+class p6{
+
+    static int sumOdd(int arr[]){
+    	int sum=0;
+
+    	for(int i=0;i<arr.length;i++){
+    		if(arr[i]%2!=0){
+             sum=sum+arr[i];
+
+    		}
+    	}
+    	return sum;
+    }
+	
+	public static void main(String[] args) {
+		
+		int arr[]={11,22,33,44,55};
+
+		System.out.println("Odd elements = ");
+
+		for(int i=0;i<arr.length;i++){
+			if(arr[i]%2!=0){
+
+				System.out.println(arr[i]+" ");
+			}
+		}
+		int result = sumOdd(arr);
+		System.out.println("\nsum ="+result);
+
+	}
+}
