f332ea5e72f5a9d3660b2cebc399197269224d7e
prasadhase31 <prasadhase9325@gmail.com>
2026-02-03 23:16:40 +0530

    packages

---
+++ src/ticketpkg/BookTicket.java
@@ -0,0 +1,17 @@
+package ticketpkg;
+import moviepkg.Movie;
+
+class BookTicket{
+	
+	public static void main(String[] args) {
+		
+		Movie m1=new Movie(1,"Ekkis","Romantic",150,10);
+
+		int tickets=6;
+		double total=m1.calculatePrice(tickets);
+		TicketBooking t1=new TicketBooking(11,101,"Aryan Shinde",6,total);
+
+		m1.displayMovieDetails();
+		t1.displayTicketDetails();
+	}
+}
---
+++ src/ticketpkg/TicketBooking.java
@@ -0,0 +1,31 @@
+package ticketpkg;
+
+public class TicketBooking{
+	
+	private int bookingId;
+	private int custId;
+	private String custName;
+	private int noOfTickets;
+	private double totalAmount;
+
+	public TicketBooking(int bookingId,int custId,String custName,int noOfTickets,double totalAmount){
+		this.bookingId=bookingId;
+		this.custId=custId;
+		this.custName=custName;
+		this.noOfTickets=noOfTickets;
+		this.totalAmount=totalAmount;
+	}
+   public int setBookingId(){
+      this.bookingId=bookingId;
+   }
+   public int getBookingId(){
+   	 return bookingId;
+   }
+   public void displayTicketDetails(){
+   	System.out.println("Booking Id :"+bookingId);
+   	System.out.println("Customer Id :"+custId);
+   	System.out.println("Customer Name :"+custId);
+   	System.out.println("Number Of Tickets :"+noOfTickets);
+   	System.out.println("Total Amount :"+totalAmount);
+   }
+}
