61f342530cbd6cf1b630624c2d274fa13d18ff31
prasadhase31 <prasadhase9325@gmail.com>
2026-02-04 00:16:23 +0530

    packages

---
+++ src/order1pkg/Order1.java
@@ -0,0 +1,27 @@
+package order1;
+
+public class Order1{
+	private int orderId;
+	private int custId;
+	private String custName;
+	private int quantity;
+	private double totalAmount;
+
+	public Order1(int orderId,int custId,String custName,int quantity,double totalAmount){
+		this.orderId=orderId;
+		this.custId=custId;
+		this.custName=custName;
+		this.quantity=quantity;
+		this.totalAmount=totalAmount;
+	}
+	
+	public void displayOrderDetails(){
+		System.out.println("Order Id :"+orderId);
+		System.out.println("Customer Id :"+custId);
+		System.out.println("Customer Name:"+custName);
+		System.out.println("Quantity:"+quantity);
+		System.out.println("Total Amount :"+totalAmount);
+
+	}
+
+}
