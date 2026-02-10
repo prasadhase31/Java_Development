0070db8b1b3ec593f5238c19c11c473bce698d77
prasadhase31 <prasadhase9325@gmail.com>
2026-02-10 13:59:47 +0530

    Polymorphism

---
+++ Polymorphism/p6.java
@@ -0,0 +1,31 @@
+//Override//
+
+class Shape{
+
+	public void draw(){
+		System.out.println("Draw Any Shape:");
+	}
+}
+ class Circle extends Shape{
+ 	public void draw(){
+ 		System.out.println("Draw Circle:");
+ 	}
+ }
+ class Rectangle extends Shape{
+ 	public void draw(){
+ 		System.out.println("Draw Rectangle:");
+ 	}
+ }
+
+class p6{
+	
+	public static void main(String[] args) {
+
+		Shape s1=new Circle();
+		Shape s2=new Rectangle();
+
+		s1.draw();
+		s2.draw();
+		
+	}
+}
