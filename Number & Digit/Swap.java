e769cd6fca15ac2c31ee75281333b1d4c2c86a6c
prasadhase31 <prasadhase9325@gmail.com>
2026-02-16 23:23:00 +0530

    Number & digit

---
+++ Number & Digit/Swap.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Swap{
+	
+	public static void main(String args[]){
+
+	  Scanner sc=new Scanner(System.in);
+
+	  System.out.println("Enter first number:");
+	  int num1=sc.nextInt();
+
+	  System.out.println("Enter second number:");
+	  int num2=sc.nextInt();
+
+	  int temp;
+
+	  temp=num1;
+	  num1=num2;
+	  num2=temp;
+
+	  System.out.println("After swap:");
+	  System.out.println("num1="+num1);
+	  System.out.println("num2="+num2);
+
+	  sc.close();
+	}
+}
