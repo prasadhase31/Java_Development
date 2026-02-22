0c755a7630d4a4bf457f213809038b3cb3de8283
prasadhase31 <prasadhase9325@gmail.com>
2026-02-22 16:45:47 +0530

    String

---
+++ String/Reverse.java
@@ -0,0 +1,13 @@
+class Reverse{
+	
+	public static void main(String[] args) {
+		
+		String str="Prasad";
+		String rev="";
+
+		for(int i=str.length()-1;i>=0;i--){
+			rev+=str.charAt(i);
+		}
+		System.out.println("Reversed:"+rev);
+	}
+}
