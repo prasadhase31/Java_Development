548bf52a4152c6cc42afaab3a94f585d10385675
prasadhase31 <prasadhase9325@gmail.com>
2026-02-22 17:20:00 +0530

    String

---
+++ String/RemoveSpaces.java
@@ -0,0 +1,10 @@
+class RemoveSpaces{
+	
+	public static void main(String[] args) {
+		
+		String str="I am learning Java ";
+
+		String result=str.replace(" ","");
+		System.out.println(result);
+	}
+}
