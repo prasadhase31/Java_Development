67997b83c86dd167bb56816423ef768e409af471
prasadhase31 <prasadhase9325@gmail.com>
2026-02-22 17:02:21 +0530

    String

---
+++ String/CountVowels.java
@@ -0,0 +1,17 @@
+class CountVowels{
+	
+	public static void main(String[] args) {
+		
+		String str="Hello Java";
+		int count=0;
+
+		for(int i=0;i<str.length();i++){
+			char ch=Character.toLowerCase(str.charAt(i));
+
+			if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+				count++;
+			}
+		}
+		System.out.println("Vowels:"+count);
+	}
+}
