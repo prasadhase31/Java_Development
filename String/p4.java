42819e982ebda4eff4888ae28ef87433f46ba875
prasadhase31 <prasadhase9325@gmail.com>
2026-02-24 23:24:07 +0530

    String

---
+++ String/p4.java
@@ -0,0 +1,15 @@
+class p4{
+	
+	public static void main(String[] args) {
+		
+		String s1="Dhoni";
+		String s2=new String("Dhoni");
+
+		if(s1.equals(s2)){
+			System.out.println("Equals");
+		}
+		else{
+			System.out.println("Not Equals");
+		}
+	}
+}
