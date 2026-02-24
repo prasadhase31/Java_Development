23074ef9f934e9e53147cf8dd82e4226406a103b
prasadhase31 <prasadhase9325@gmail.com>
2026-02-24 23:18:02 +0530

    String

---
+++ String/p3.java
@@ -0,0 +1,15 @@
+class p3{
+	
+	public static void main(String[] args) {
+		
+		String s1=new String("Dhoni");
+		String s2=new String("Dhoni");
+
+		if(s1==s2){
+			System.out.println("Same");
+		}
+		else{
+			System.out.println("Not Same");
+		}
+	}
+}
