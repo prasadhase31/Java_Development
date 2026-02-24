bad554d484046f50a3ebe810eb992142ebc5a995
prasadhase31 <prasadhase9325@gmail.com>
2026-02-24 23:12:49 +0530

    String

---
+++ String/p2.java
@@ -0,0 +1,15 @@
+class p2{
+	
+	public static void main(String[] args) {
+		
+		String s1="Virat";
+		String s2="Virat";
+
+		if(s1==s2){
+			System.out.println("Same");
+		}
+		else{
+			System.out.println("Not Same");
+		}
+	}
+}
