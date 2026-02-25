9560baef2bf4c35dd7f3f70049a96edc0d27362e
prasadhase31 <prasadhase9325@gmail.com>
2026-02-25 14:45:26 +0530

    String

---
+++ String/p5.java
@@ -0,0 +1,15 @@
+class p5{
+	
+	public static void main(String[] args) {
+		
+		String s1=new String("Rohit");
+		String s2=new String("Rohit");
+
+		if(s1.equals(s2)){
+			System.out.println("Equals");
+		}
+		else{
+			System.out.println("Not Equals");
+		}
+	}
+}
