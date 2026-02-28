baac091f447de2d17d32dcc896a2a9397a05910e
prasadhase31 <prasadhase9325@gmail.com>
2026-02-28 23:52:48 +0530

    via update

---
+++ String/Duplicate.java
@@ -0,0 +1,20 @@
+public class Duplicate {
+    public static void main(String[] args) {
+
+        String str = "programming";
+
+        char[] ch = str.toCharArray();
+
+        System.out.println("Duplicate characters are:");
+
+        for(int i = 0; i < ch.length; i++) {
+            for(int j = i + 1; j < ch.length; j++) {
+
+                if(ch[i] == ch[j]) {
+                    System.out.println(ch[j]);
+                    break;   // duplicate ekdach print hoil
+                }
+            }
+        }
+    }
+}
