b8557a2531c2e02399042e302031faaf55b5561b
prasadhase31 <prasadhase9325@gmail.com>
2026-03-02 14:29:54 +0530

    String

---
+++ String/LowerToUpper.java
@@ -0,0 +1,14 @@
+/*Write a Java  program to convert lowercase string to uppercase.*/
+
+class LowerToUpper{
+	
+	public static void main(String[] args) {
+		
+		String s1="i love java programming";
+
+		String upper=s1.toUpperCase();
+
+		System.out.println("Original String:"+s1);
+		System.out.println("Uppercase String:"+upper);
+	}
+}
