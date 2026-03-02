6bc701ef821218146445f02f770d92b150614cae
prasadhase31 <prasadhase9325@gmail.com>
2026-03-02 12:53:33 +0530

    String

---
+++ String/Copy.java
@@ -0,0 +1,14 @@
+/*Write a Java  program to copy one string to another string. */
+
+class Copy{
+	
+	public static void main(String[] args) {
+		
+		String s1="I do Coding";
+
+		String s2=s1;
+
+		System.out.println("Original String:"+s1);
+		System.out.println("Copy String:"+s2);
+	}
+}
