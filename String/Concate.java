cb7cddf40044539d11c9c2aa2714763fa4b37cb4
prasadhase31 <prasadhase9325@gmail.com>
2026-03-02 12:58:59 +0530

    String

---
+++ String/Concate.java
@@ -0,0 +1,14 @@
+/*Write a Java  program to concatenate two strings.*/
+
+class Concate{
+	
+	public static void main(String[] args) {
+		
+		String s1="Virat";
+		String s2="Kohli";
+
+		String result=s1.concat(s2);
+
+		System.out.println("Concatenated String:"+result);
+	}
+}
