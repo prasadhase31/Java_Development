ed69cea06db3565fc88c4ad4a8598ae0cb71887c
prasadhase31 <prasadhase9325@gmail.com>
2026-03-10 16:26:15 +0530

    MultiThreading

---
+++ MultiThreading/BankTransaction.java
@@ -0,0 +1,28 @@
+class BankTransaction extends Thread{
+	
+	private String transctionName;
+
+	BankTransaction(String name){
+		this.transctionName=name;
+	}
+	public void run(){
+		System.out.println(transctionName+ "Started");
+	try{
+		Thread.sleep(3000);
+	}
+	catch(InterruptedException e){
+		e.printStackTrace();
+	}
+	System.out.println(transctionName+ "Completed");
+}
+
+	public static void main(String[] args) {
+		BankTransaction b1=new BankTransaction("Deposit");
+		BankTransaction b2=new BankTransaction("Withdraw");
+
+		b1.start();
+		b2.start();
+		
+
+	}
+}
