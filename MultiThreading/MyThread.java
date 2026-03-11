e9092830cd3f1d3d6f7a92050d89d10b5141a7f5
prasadhase31 <prasadhase9325@gmail.com>
2026-03-11 13:49:00 +0530

    MultiThreading

--- MultiThreading/MyThread.java
+++ MultiThreading/MyThread.java
@@ -10,10 +10,10 @@ class MyThread extends Thread{
 	public static void main(String args[]){
 
 		MyThread t1=new MyThread("Thread 1",3);
-		MyThread t2=new MyThread("Thread 2",7);
+		MyThread t2=new MyThread("Thread 2",3);
 		MyThread t3=new MyThread("Thread 3",5);
 		MyThread t4=new MyThread("Thread 4",7);
-		MyThread t5=new MyThread("Thread 5",4);
+		MyThread t5=new MyThread("Thread 5",5);
 
 		t1.start();
 		t2.start();
