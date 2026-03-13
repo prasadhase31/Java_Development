fa10e5fcbcb238f6bf067555ebfcf7ece04a2fe5
prasadhase31 <prasadhase9325@gmail.com>
2026-03-13 20:46:10 +0530

    MultiThreading

--- MultiThreading/Synchronized1.java
+++ MultiThreading/Synchronized1.java
@@ -1,28 +1,14 @@
-class Bank{
-	int balance=1000;
-    
-    synchronized(this){
-    balance = balance - amount;
-}
-	
+class Demo{
+	void printNumbers(){
+
+		synchronized(this){
+			for(int i=1;i<=5;i++){
+				System.out.println(i);
+			}
+		}
+	}
 }
 class Synchronized1 extends Thread{
-	Bank b;
 
-	Synchronized(Bank b){
-		this.b=b;
-	}
-	public void run(){
-		b.withdraw(500);
-	}
-	public static void main(String[] args) {
-		
-		Bank b1=new Bank();
-
-		Synchronized s1=new Synchronized(b1);
-		Synchronized s2=new Synchronized(b1);
-
-		s1.start();
-		s2.start();
-	}
+	Demo d;
 }
