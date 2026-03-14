ae60213ace31c894bfc3d4eba75fd7f809b0d0cf
prasadhase31 <prasadhase9325@gmail.com>
2026-03-14 23:13:56 +0530

    MultiThreading

---
+++ MultiThreading/Deadlock.java
@@ -0,0 +1,16 @@
+class Deadlock{
+		
+		static final Object lock1=new Object();
+		static final Object lock2=new object();
+
+		public static void main(String[] args) {
+			
+			Thread t1=new Thread(){
+				public void run(){
+					synchronized(lock1){
+						System.out.println("Thread 1:locked lock1");
+					}
+				}
+			}
+		}
+	}
