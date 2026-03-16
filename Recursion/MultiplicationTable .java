3c1a26cd35326ae6e28138e2615c2d72c4b5b093
prasadhase31 <prasadhase9325@gmail.com>
2026-03-16 23:37:56 +0530

    Recursion

---
+++ Recursion/MultiplicationTable .java
@@ -0,0 +1,19 @@
+public class MultiplicationTable {
+
+    static void printTable(int n, int i) {
+
+        if(i > 10)     // base condition
+            return;
+
+        System.out.println(n + " x " + i + " = " + (n*i));
+
+        printTable(n, i+1);   // recursive call
+    }
+
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        printTable(n, 1);
+    }
+}
