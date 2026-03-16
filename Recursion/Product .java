38d0e918dc5bed04925317a3b544b0f2ad40a86e
prasadhase31 <prasadhase9325@gmail.com>
2026-03-16 23:26:04 +0530

    Recursion

---
+++ Recursion/Product .java
@@ -0,0 +1,21 @@
+public class Product {
+
+    static int multiply(int a, int b) {
+
+        if(b == 0)        // base condition
+            return 0;
+
+        return a + multiply(a, b-1);   // recursive call
+    }
+
+    public static void main(String[] args) {
+
+        int a = 4;
+        int b = 3;
+
+        int result = multiply(a, b);
+
+        System.out.println("Product is: " + result);
+    }
+}
+	
