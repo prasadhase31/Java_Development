91fafe78f95e68c218911c8160951952b4322cf2
prasadhase31 <prasadhase9325@gmail.com>
2026-03-17 10:25:28 +0530

    Recursion

---
+++ Recursion/Numbers1.java
@@ -0,0 +1,14 @@
+class Numbers1{
+	
+public static void printInc(int n){
+	if(n==5)
+		return 0;
+	else
+     System.out.println(n+" ");
+     printInc(n-1);
+}
+
+	public static void main(String[] args) {
+		
+	}
+}
