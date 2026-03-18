fefdf82a04afc1e5304ae63db9d90d80c1cd93cc
prasadhase31 <prasadhase9325@gmail.com>
2026-03-18 11:02:59 +0530

    MultiThreading

---
+++ Recursion/Reverse.java
@@ -0,0 +1,16 @@
+class Reverse{
+	
+	public static int rev(int n){
+
+	  if(n==0)
+
+	  return rev;
+
+	 return rev(n/10,rev*10,n);
+	}
+	public static void main(String args[]){
+
+	int n=12345;
+	System.out.println("Reverse:"+rev(n,0));
+	}
+}
