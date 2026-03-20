c2382f5e8c3f190c9c0e437f04fffb596a027278
prasadhase31 <prasadhase9325@gmail.com>
2026-03-20 22:19:21 +0530

    Recursion

--- Recursion/SumDigits.java
+++ Recursion/SumDigits.java
@@ -4,5 +4,11 @@ class SumDigits{
 
 		if (n==0)
 			return 0;
+
+		return (n%10)+sumDigits(n/10);
+	}
+	public static void main(String[] args) {
+		int num=1234;
+		System.out.println("Sum of digits:"+sumDigits(num));
 	}
 }
