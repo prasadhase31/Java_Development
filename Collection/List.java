7c7967061cc40b9571d18ab554809af1df95ee3c
prasadhase31 <prasadhase9325@gmail.com>
2026-03-30 14:40:22 +0530

    Collection

---
+++ Collection/List.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class List{
+	
+    public static void main(String[] args) {
+    	
+    	ArrayList<Integer> list=new ArrayList<>();
+
+    	list.add(10);
+    	list.add(20);
+    	list.add(30);
+    	list.add(40);
+    	list.add(50);
+
+    	System.out.println(list);
+    }
+}
