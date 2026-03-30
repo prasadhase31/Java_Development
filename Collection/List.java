5330d6d05474f1be6ccc7e1fbec1effe69dc4a20
prasadhase31 <prasadhase9325@gmail.com>
2026-03-30 15:04:40 +0530

    Collection

--- Collection/List.java
+++ Collection/List.java
@@ -12,6 +12,17 @@ public class List{
     	list.add(40);
     	list.add(50);
 
-    	System.out.println(list);
+    	list.remove(3);
+
+    	list.size();
+
+    	System.out.println(list.size());
+
+    	System.out.println("Element at index 2:"+list.get(2));
+
+    	list.set(1,80);
+    	System.out.println("After set(1,80):"+list);
+
+    	System.out.println(list.contains(59));
     }
 }
