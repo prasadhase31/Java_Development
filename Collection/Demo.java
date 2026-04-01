55c0690b8af73fad6a77109fe5274e308189ca66
prasadhase31 <prasadhase9325@gmail.com>
2026-04-01 14:17:12 +0530

    Collection

---
+++ Collection/Demo.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Demo{
+
+	public static void main(String[] args) {
+		
+		LinkedList<Integer> list=new LinkedList<>();
+
+		list.add(12);
+		list.add(24);
+		list.
+
+	}
+	
+
+}
