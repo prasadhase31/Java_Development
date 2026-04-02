9838f52a0f72c65f58c3074ada6adbd58873061b
prasadhase31 <prasadhase9325@gmail.com>
2026-04-02 12:48:05 +0530

    Collection

---
+++ Collection/NegativeCount.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class NegativeCount{
+	
+	public static void main(String[] args) {
+		
+		ArrayList<Integer> list=new ArrayList<>();
+
+		list.add(87);
+		list.add(-32);
+		list.add(-2);
+		list.add(3);
+
+		int count=0;
+
+		for(int num:list){
+           if(num<0){
+           	count++;
+           }
+
+		}
+		System.out.println("Negative Element:"+count);
+	}
+}
