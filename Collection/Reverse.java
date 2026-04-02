fbd3928f7e23674a3117abcfe757bb35677c69d1
prasadhase31 <prasadhase9325@gmail.com>
2026-04-02 13:39:58 +0530

    Collection

---
+++ Collection/Reverse.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Reverse{
+	
+	public static void main(String[] args) {
+		
+		ArrayList<Integer>list =new ArrayList<>();
+
+		list.add(2);
+		list.add(8);
+		list.add(6);
+		list.add(3);
+System.out.println("Original List:"+list);
+
+		}
+	}
