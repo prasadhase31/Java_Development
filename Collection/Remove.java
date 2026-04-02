8fde86c2110f21bea1c9963e876e118808773ca7
prasadhase31 <prasadhase9325@gmail.com>
2026-04-02 13:25:28 +0530

    Collection

---
+++ Collection/Remove.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Remove{
+	 
+	 public static void main(String[] args) {
+	 	
+	 	ArrayList<Integer>list=new ArrayList<>();
+
+	 	list.add(43);
+	 	list.add(76);
+	 	list.add(23);
+	 	list.add(89);
+	 	list.add(11);
+
+	 	System.out.println("Original List:"+list);
+
+	 	list.remove(3);
+
+	 	System.out.println("Updaed List:"+list);
+
+	 }
+}
