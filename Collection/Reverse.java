abe466e88e5e6f299195a732e91fb567cdedbec8
prasadhase31 <prasadhase9325@gmail.com>
2026-04-02 14:28:50 +0530

    Collection

--- Collection/Reverse.java
+++ Collection/Reverse.java
@@ -9,7 +9,15 @@ class Reverse{
 		list.add(8);
 		list.add(6);
 		list.add(3);
-System.out.println("Original List:"+list);
+
+     System.out.println("Original List:"+list);
+
+     ArrayList<Integer>reverseList=new ArrayList<>();
+
+      for(int i=list.size()-1;i>=0;i--){
+      	 reverseList.add(list.get(i));
+      }
+      System.out.println("Reversed List:"+reverseList);
 
 		}
 	}
