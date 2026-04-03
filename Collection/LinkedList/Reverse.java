8540c284f9447de6c7fce3c616c90705fb2753b8
prasadhase31 <prasadhase9325@gmail.com>
2026-04-03 14:36:51 +0530

    LinkedList

--- Collection/LinkedList/Reverse.java
+++ Collection/LinkedList/Reverse.java
@@ -11,5 +11,12 @@ class Reverse{
     	list.add(32);
 
     	System.out.println("Original Elements:"+list);
+
+    	LinkedList<Integer> reverse=new LinkedList<>();
+
+    	for(int i=list.size()-1;i>=0;i--){
+    		reverse.add(list.get(i));
+    	}
+    	System.out.println("Reverse Elements:"+reverse);
     }
 }
