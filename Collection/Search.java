2769da9945a8887724d6f5bb341fef1b6f8c5be3
prasadhase31 <prasadhase9325@gmail.com>
2026-04-03 09:09:00 +0530

    Collection

---
+++ Collection/Search.java
@@ -0,0 +1,36 @@
+import java.util.*;
+class Search{
+	
+	public static void main(String[] args) {
+		
+		//Scanner sc=new Scanner(System.in);
+
+     ArrayList<Integer> list=new ArrayList<>();
+
+     list.add(35);
+     list.add(40);
+     list.add(50);
+     list.add(55);
+     list.add(80);
+
+     int key=40;
+
+     boolean found=false;
+
+     for(int num:list){
+     	if(num==key){
+     		found=true;
+     		break;
+     	}
+     }
+     if(found){
+     	System.out.println("Element found:");
+     }
+     else{
+     	System.out.println("Element not found");
+     }
+   
+
+	}
+
+}
