7130c2f99f8c2f01fa48ab63bb1c9cff34ba20de
prasadhase31 <prasadhase9325@gmail.com>
2026-04-04 17:53:49 +0530

    LinkedList

---
+++ Collection/LinkedList/Even.java
@@ -0,0 +1,21 @@
+class Even{
+	
+	public static void main(String[] args) {
+		
+		LinkedList<Integer> list=new LinkedList<>();
+
+		list.add(78);
+		list.add(32);
+		list.add(21);
+		list.add(77);
+
+		int count=0;
+
+		for(int num:list){
+			if(num % 2==0){
+             count++;
+			}
+		}
+		System.out.println("Even count:"+count);
+	}
+}
