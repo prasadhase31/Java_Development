657f09f88d633193856b3698f35cbc066001c424
prasadhase31 <prasadhase9325@gmail.com>
2026-04-05 16:04:23 +0530

    Stack

---
+++ Collection/Stack/Demo.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+class Demo{
+	
+	public static void main(String[] args) {
+		
+		Stack<Integer> stack=new Stack<>();
+
+		stack.push(10);
+		stack.push(20);
+		stack.push(30);
+
+		System.out.println(stack);
+	}
+}
