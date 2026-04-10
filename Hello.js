a15fa12302fe65570efa6ea9372f3e65d252a6be
prasadhase31 <prasadhase9325@gmail.com>
2026-04-10 10:29:37 +0530

    via upload

--- Hello.js
+++ Hello.js
@@ -1,7 +1,19 @@
 let btn = document.querySelector("button");
+let inp = document.querySelector("input");
 
-btn.addEventListener("click",function(){
+btn.addEventListener("click", function () {
     console.log(this);
     this.style.backgroundColor = "red"
     console.log("click the button");
+});
+
+inp.addEventListener("keydown", function () {
+    console.log("key", event.key);
+    console.log("code", event.code);
+    console.log("key was pressed");
+});
+
+inp.addEventListener("keyup", function () {
+
+    console.log("key was released");
 });
