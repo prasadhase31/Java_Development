dec9782dcb914d9dfc3e1dac54acfe901ef62f74
prasadhase31 <prasadhase9325@gmail.com>
2026-04-11 21:02:36 +0530

    via upload

--- Hello.js
+++ Hello.js
@@ -1,19 +1,40 @@
-let btn = document.querySelector("button");
-let inp = document.querySelector("input");
+// let btn = document.querySelector("button");
+// let inp = document.querySelector("input");
 
-btn.addEventListener("click", function () {
-    console.log(this);
-    this.style.backgroundColor = "red"
-    console.log("click the button");
-});
+// btn.addEventListener("click", function () {
+//     console.log(this);
+//     this.style.backgroundColor = "red"
+//     console.log("click the button");
+// });
 
-inp.addEventListener("keydown", function () {
-    console.log("key", event.key);
+inp.addEventListener("keydown", function ( ) {
+    console.log("key", event.key); 
     console.log("code", event.code);
+     console.log("Key Code", event.keyCode);
     console.log("key was pressed");
 });
 
 inp.addEventListener("keyup", function () {
 
     console.log("key was released");
+});
+
+inp.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        console.log("Enter Pressed!");
+    }
+});
+
+
+
+inp.addEventListener("keyup", function(event) {
+    console.log("You typed:", event.target.value);
+});
+
+
+let form = document.querySelector("form");
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault(); // stop page reload
+    console.log("Form Submitted");
 });
