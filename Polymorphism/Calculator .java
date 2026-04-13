0a3850fe76de77e0ce31908875866eae421d4794
prasadhase31 <prasadhase9325@gmail.com>
2026-04-13 23:56:21 +0530

    via upload

---
+++ Polymorphism/Calculator .java
@@ -0,0 +1,11 @@
+class Calculator {
+
+    // Method with 2 parameters
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+    
