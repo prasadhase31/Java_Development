52b609d5bc6f28cb1c327a37dcd143dfe6d396e9
prasadhase31 <prasadhase9325@gmail.com>
2026-04-14 23:52:40 +0530

    via upload

--- Constructor/Singleton.java
+++ Constructor/Singleton.java
@@ -14,3 +14,9 @@ class Singleton {
         return instance;
     }
 }
+
+public class Main {
+    public static void main(String[] args) {
+
+        Singleton obj1 = Singleton.getInstance();
+        Singleton obj2 = Singleton.getInstance();
