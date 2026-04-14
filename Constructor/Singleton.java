b65433ce1b7accccfcd0c601a51fa098cf31ecb1
prasadhase31 <prasadhase9325@gmail.com>
2026-04-14 23:50:07 +0530

    via upload

--- Constructor/Singleton.java
+++ Constructor/Singleton.java
@@ -2,3 +2,12 @@ class Singleton {
 
     // Step 1: static variable
     private static Singleton instance;
+
+    // Step 2: private constructor
+    private Singleton() {}
+
+    // Step 3: public method
+    public static Singleton getInstance() {
+        if(instance == null) {
+            instance = new Singleton();
+        }
