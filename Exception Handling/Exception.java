13cd121c2a7e79c013dad6ff5a507a7b9caeaf5e
prasadhase31 <prasadhase9325@gmail.com>
2026-04-17 12:04:03 +0530

    Exception Handling

--- Exception Handling/Exception.java
+++ Exception Handling/Exception.java
@@ -4,6 +4,13 @@ class Exception{
 		String str=null;
 
 		try{
+			System.out.println(str.length());
+		}
+		catch(NullPointerException e){
+			System.out.println("NullPointerException:"+e.getMessage());
+		}
+		int arr[]={1,2,3};
+		try{
 			System.out.println()
 		}
 	}
