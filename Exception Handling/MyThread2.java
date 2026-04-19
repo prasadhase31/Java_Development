8ec77a648ea42929c674a08e01e7762ef2de918c
prasadhase31 <prasadhase9325@gmail.com>
2026-04-19 18:39:00 +0530

    Exception Handling

--- Exception Handling/MyThread2.java
+++ Exception Handling/MyThread2.java
@@ -1,17 +1,13 @@
 class MyThread2 extends Thread{
-	
-	public MyThread2(){
 
 		public void run(){
 			System.out.println("Thread is running");
 		}
-	}
+	
 	public static void main(String[] args) {
 		
 		MyThread2 m=new MyThread2();
          
-
          m.start();
-
+}	
 	}
-}
