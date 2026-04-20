2870cb8d78e92f245b22e953a482abeba908ee07
prasadhase31 <prasadhase9325@gmail.com>
2026-04-20 14:05:24 +0530

    Exception Handling

--- MultiThreading/MyDaemon.java
+++ MultiThreading/MyDaemon.java
@@ -8,5 +8,14 @@ class MyDaemon extends Thread{
 			System.out.println("User therad running");
 		}
 	}
-	
+	public static void main(String[] args) {
+		
+		MyDaemon m1=new MyDaemon();
+		MyDaemon m2=new MyDaemon();
+
+		t1.setDaemon(true);
+
+		t1.start();
+		t2.start();
+	}
 }
