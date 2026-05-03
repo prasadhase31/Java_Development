7df1e4b63630d574a37368b26ff5fe5c22ae9707
prasadhase31 <prasadhase9325@gmail.com>
2026-05-03 18:14:32 +0530

    Encapsulation

--- Encapsulation/BankAccount.java
+++ Encapsulation/BankAccount.java
@@ -15,5 +15,20 @@ class BankAccount{
    public void withdraw(double amount){
    	  balance -= amount;
    }
+   
+   public double getBalance(){
+   	   return balance;
+   }
+}
+public class Main{
 
+	public static void main(String[] args) {
+		
+		BankAccount b1=new BankAccount(1000);
+
+		b1.deposit(500);
+		b1.withdraw(2000);
+
+		System.out.println("Balance:"+b1.getBalance());
+	}
 }
