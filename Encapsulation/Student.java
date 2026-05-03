1546e588322593e6dff7cb1adc9166246dfb6808
prasadhase31 <prasadhase9325@gmail.com>
2026-05-03 17:41:51 +0530

    Encapsulation

---
+++ Encapsulation/Student.java
@@ -0,0 +1,19 @@
+class Student{
+	
+	private String name;
+	private int age;
+	private int rollno;
+
+	Student(String name,int age,int rollno){
+		this.name;
+		this.age;
+		this.rollno;
+	}
+	public String getName(){
+		return name;
+	}
+
+	public void setName(String name){
+		this.name=name;
+	}
+}
