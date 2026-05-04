a82955248e5dde443f72697571d86091af234a7a
prasadhase31 <prasadhase9325@gmail.com>
2026-05-04 14:49:55 +0530

    Map

---
+++ Collection/Map/Demo.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Demo{
+	
+	public static void main(String[] args) {
+		
+		Map<Integer,String>map=new HashMap<>();
+
+		map.put(101,"Prasad");
+		map.put(102,"Siddhu");
+		map.put(103,"Sharvil");
+
+		System.out.println(map);
+
+		System.out.println(map.get(102));
+
+		map.remove(103);
+
+		System.out.println(map);
+
+
+	}
+}
