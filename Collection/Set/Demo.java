5f60036ae941336775c3c15127d3a7343fb8533a
prasadhase31 <prasadhase9325@gmail.com>
2026-05-05 10:57:11 +0530

    Set

---
+++ Collection/Set/Demo.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Demo{
+	
+	public static void main(String[] args) {
+		
+    HashSet<String>set=new HashSet<>();
+
+    set.add("Prasad");
+    set.add("Siddhu");
+    set.add("Sharvil");
+    set.add("Shubh");
+
+    System.out.println(set);
+	}
+}
