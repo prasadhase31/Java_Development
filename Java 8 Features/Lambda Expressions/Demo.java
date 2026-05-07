5ec8283b43fe0f5b98a4878012a4d668568c414d
prasadhase31 <prasadhase9325@gmail.com>
2026-05-07 17:49:28 +0530

    Java 8

--- Java 8 Features/Lambda Expressions/Demo.java
+++ Java 8 Features/Lambda Expressions/Demo.java
@@ -3,9 +3,15 @@ class Demo{
 	
 	public static void main(String[] args) {
 		
-		Predicate<Integer> p=x -> x>10;
+		//Predicate<Integer> p=x -> x>10;
 
-		System.out.println(p.test(20));
-		System.out.println(p.test(5));
+		//System.out.println(p.test(20));
+		//System.out.println(p.test(5));
+        
+        Predicate<Integer>p1=x -> x>10;
+        Predicate<Integer>p2=x -> x<50;
+
+        System.out.println(p1.and(p2).test(20));
+
 	}
 }
