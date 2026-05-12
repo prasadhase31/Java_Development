c0e2158d2a675b06c56384d4ddc904e7e2550c09
prasadhase31 <prasadhase9325@gmail.com>
2026-05-12 10:57:29 +0530

    Java 8

---
+++ JDBC/NativeAPI.java
@@ -0,0 +1,15 @@
+class NativeAPI{
+
+	public static void main(String[] args) {
+		
+		try{
+
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+			Connection con=DriverManager.getConnection(
+				"")
+		}
+	}
+	
+
+}
