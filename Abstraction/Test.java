de9a48f50297a7aa921d445a751cf69e52bd32d7
prasadhase31 <prasadhase9325@gmail.com>
2026-05-20 11:05:28 +0530

    Abstraction

--- Abstraction/Test.java
+++ Abstraction/Test.java
@@ -5,3 +5,18 @@ abstract class Animal{
 		System.out.println("Animal Constructor");
 	}
 }
+
+class Dog extends Animal{
+
+	Dog(){
+
+		System.out.println("Dog Constructor");
+	}
+
+}
+class Test{
+
+	public static void main(String[] args) {
+		
+	}
+}
