07bea89bf726eb64870c0d1148de232e4fb83e13
prasadhase31 <prasadhase9325@gmail.com>
2026-06-12 16:27:47 +0530

    Servlet

--- Servlet/src/main/java/Practice.java
+++ Servlet/src/main/java/Practice.java
@@ -2,7 +2,7 @@ import java.io.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.ServletException;
-
+import jakarta.servlet.ServletConfig;
 
 @WebServlet("/welcome")
 public class Practice extends HttpServlet {
@@ -10,6 +10,9 @@ public class Practice extends HttpServlet {
 	protected void doGet(HttpServletRequest req,HttpServletResponse res)
 	     throws IOException,ServletException{
 		
+		ServletConfig config=getServletConfig();
+		
+		String str=config.getServlet
 		res.getWriter().println("Welcome");
 	}
 }
