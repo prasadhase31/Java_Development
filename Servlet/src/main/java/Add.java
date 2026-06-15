660ed3c2042b72b397100021ee13493608345a28
prasadhase31 <prasadhase9325@gmail.com>
2026-06-15 11:05:58 +0530

    Servlet

--- Servlet/src/main/java/Add.java
+++ Servlet/src/main/java/Add.java
@@ -1,4 +1,11 @@
-
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
 public class Add extends HttpServlet {
+	
+	public void doGet(HttpServletRequest req,HttpServletresponse res)throws IOException,ServletException{
+		
+		
+	}
 
 }
