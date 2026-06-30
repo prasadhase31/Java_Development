6f6fd82e9ba07ce975632fb5e47032d3870fca73
prasadhase31 <prasadhase9325@gmail.com>
2026-06-30 11:03:13 +0530

    Servlet And JSP

--- Session/src/main/java/HomeServlet.java
+++ Session/src/main/java/HomeServlet.java
@@ -2,5 +2,26 @@ import java.io.*;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 public class HomeServlet extends HttpServlet {
-
+    protected void doGet(HttpServletRequest req,HttpServletResponse res)throws ServletException,IOException{
+    	
+    	HttpSession session=req.getSession(false);
+    	
+    	res.setContentType("text/html");
+    	PrintWriter out=res.getWriter();
+    	
+    	if(session !=null) {
+    		String user=(String)session.getAttribute("user");
+    	
+    	if(user !=null) {
+    		out.println("<h1>Welcome"+user+"</h1>");
+    	}
+    	
+    	else {
+    		out.println("Please Login");
+    	}
+    }
+    else {
+    	out.println("Session Expired");
+    }
+}
 }
