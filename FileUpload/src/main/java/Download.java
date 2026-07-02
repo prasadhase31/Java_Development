a6daa96a57493bf88cc7c5044de1a6140b80edc9
prasadhase31 <prasadhase9325@gmail.com>
2026-07-02 18:34:38 +0530

    File Upload

--- FileUpload/src/main/java/Download.java
+++ FileUpload/src/main/java/Download.java
@@ -13,6 +13,16 @@ public class Download extends HttpServlet {
 		String path=getServletContext().getRealPath("")+"uploads + File.separtor + fileName";
 		
 		File file=new File(path);
+		
+		res.setContentType("application/pdf");
+		
+		res.setHeader("Content-Disposition","attachment;filename="+file.getName());
+		
+		FileInputStream fis=new FileInputStream(file);
+		
+		OutputStream os=res.getOutputStream();
+		
+		byte[] buffer=new byte[4096];
 	}
 
 }
