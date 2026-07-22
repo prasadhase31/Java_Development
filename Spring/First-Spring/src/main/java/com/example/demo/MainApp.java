526f81e9e5801009430da319e4f23a6f31ab802d
prasadhase31 <prasadhase9325@gmail.com>
2026-07-22 15:42:48 +0530

    Spring

--- Spring/First-Spring/src/main/java/com/example/demo/MainApp.java
+++ Spring/First-Spring/src/main/java/com/example/demo/MainApp.java
@@ -1,7 +1,13 @@
 package com.example.demo;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class MainApp {
    public static void main() {
 
+       ApplicationContext context=new ClassPathXmlApplicationContext("applicationBeanContext.xml");
+
+       GreetingService greetingService=(GreetingService) context.getBean("myBean");
     }
 }
