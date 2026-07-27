0d38969fa680744401c9a7097475687e501c2994
prasadhase31 <prasadhase9325@gmail.com>
2026-07-27 21:56:17 +0530

    Spring-Practice

---
+++ Spring/SpringAnnotationPractice/src/main/java/com/example/annotations/Laptop.java
@@ -0,0 +1,12 @@
+package com.example.practice;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+
+    public void start() {
+        System.out.println("Laptop Started...");
+    }
+
+}
