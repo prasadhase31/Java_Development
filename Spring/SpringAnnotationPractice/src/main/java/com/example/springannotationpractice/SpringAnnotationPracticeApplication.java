3c8b5416b52cd1ffb68cc0c4bf86836b8b973c97
prasadhase31 <prasadhase9325@gmail.com>
2026-07-28 15:07:03 +0530

    Spring-Annotations

---
+++ Spring/SpringAnnotationPractice/src/main/java/com/example/springannotationpractice/SpringAnnotationPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.springannotationpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAnnotationPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAnnotationPracticeApplication.class, args);
+    }
+
+}
---
+++ Spring/SpringAnnotationPractice/src/test/java/com/example/springannotationpractice/SpringAnnotationPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springannotationpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAnnotationPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
