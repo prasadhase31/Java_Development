a089953e1a954bbe92a4fb84a25a7e6eeaf4994d
prasadhase31 <prasadhase9325@gmail.com>
2026-07-29 20:52:33 +0530

    Spring-Annotations

--- Spring/SpringAnnotationPractice/src/main/java/com/example/springannotationpractice/SpringAnnotationPracticeApplication.java
+++ Spring/SpringAnnotationPractice/src/main/java/com/example/springannotationpractice/SpringAnnotationPracticeApplication.java
@@ -2,12 +2,18 @@ package com.example.springannotationpractice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringAnnotationPracticeApplication {
 
     public static void main(String[] args) {
+
+        ApplicationContext context=
         SpringApplication.run(SpringAnnotationPracticeApplication.class, args);
+
+        Student student=context.getBean(Student.class);
+        student.study();
     }
 
 }
--- Spring/SpringAnnotationPractice/src/main/java/com/example/springannotationpractice/Student.java
+++ Spring/SpringAnnotationPractice/src/main/java/com/example/springannotationpractice/Student.java
@@ -10,7 +10,14 @@ public class Student {
 
     @Autowired
     private Laptop laptop;
+
+    @Autowired
+    private Printer printer;
     public void study(){
+
+        laptop.start();
+
+        printer.print();
         System.out.println("Student is Studying Spring");
     }
 }
