bb6e52fe32f9f38919e663eec486a0e5d72d10ff
prasadhase31 <prasadhase9325@gmail.com>
2026-07-29 22:26:59 +0530

    Spring-Annotations

--- Spring/SpringAnnotationPractice/src/main/java/com/example/springannotationpractice/Student.java
+++ Spring/SpringAnnotationPractice/src/main/java/com/example/springannotationpractice/Student.java
@@ -3,8 +3,6 @@ package com.example.springannotationpractice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.sql.SQLOutput;
-
 @Component
 public class Student {
 
@@ -13,6 +11,8 @@ public class Student {
 
     @Autowired
     private Printer printer;
+
+
     public void study(){
 
         laptop.start();
