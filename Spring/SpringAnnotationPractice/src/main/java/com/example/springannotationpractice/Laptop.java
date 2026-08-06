490f4704d8c0bd3ee397b2f2ebf1aa45c735c539
prasadhase31 <prasadhase9325@gmail.com>
2026-08-06 09:33:57 +0530

    Spring-Annotations

--- Spring/SpringAnnotationPractice/src/main/java/com/example/springannotationpractice/Laptop.java
+++ Spring/SpringAnnotationPractice/src/main/java/com/example/springannotationpractice/Laptop.java
@@ -1,10 +1,15 @@
 package com.example.springannotationpractice;
 
 import org.springframework.stereotype.Component;
-
+import org.springframework.context.annotation.Scope;
 @Component
+@Scope("prototype")
+
 public class Laptop {
 
+    public Laptop(){
+        System.out.println("Laptop Object Created");
+    }
     public void start(){
         System.out.println("Laptop Started");
     }
