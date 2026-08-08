89f5758ff609b33ca7d6b899d9bb5b8f7bd8701b
prasadhase31 <prasadhase9325@gmail.com>
2026-08-08 16:10:12 +0530

    Spring-Annotations

--- Spring/SpringAnnotationPractice/src/main/java/com/example/springannotationpractice/service/StudentService.java
+++ Spring/SpringAnnotationPractice/src/main/java/com/example/springannotationpractice/service/StudentService.java
@@ -2,6 +2,7 @@ package com.example.springannotationpractice.service;
 
 import com.example.springannotationpractice.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,12 +11,23 @@ public class StudentService {
     @Autowired
     private StudentRepository repository;
 
+    @Value("${student.name}")
+    private String studentName;
+
+    @Value("${student.course}")
+    private String course;
+
+    @Value("${student.age}")
+    private int age;
+
     public void registerStudent() {
+
+        System.out.println("Student Name: " + studentName);
+        System.out.println("Course: " + course);
+        System.out.println("Age: " + age);
 
         System.out.println("Business Logic Executed");
 
         repository.saveStudent();
-
     }
-
 }
