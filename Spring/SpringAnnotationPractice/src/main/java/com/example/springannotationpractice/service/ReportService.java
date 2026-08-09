a50c9241e05a5e4258bc0547a1bf067ad300907f
prasadhase31 <prasadhase9325@gmail.com>
2026-08-09 11:27:28 +0530

    Spring-Annotations

---
+++ Spring/SpringAnnotationPractice/src/main/java/com/example/springannotationpractice/service/ReportService.java
@@ -0,0 +1,13 @@
+package com.example.springannotationpractice.service;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Service;
+
+@Service
+@Lazy
+public class ReportService {
+
+    public ReportService(){
+        System.out.println("ReportService Object Created");
+    }
+}
