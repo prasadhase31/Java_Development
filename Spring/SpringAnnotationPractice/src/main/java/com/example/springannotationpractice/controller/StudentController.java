31e0fee363598ab6808c53d3e2a2564d49b01f20
prasadhase31 <prasadhase9325@gmail.com>
2026-08-11 21:15:11 +0530

    Spring-Annotations

--- Spring/SpringAnnotationPractice/src/main/java/com/example/springannotationpractice/controller/StudentController.java
+++ Spring/SpringAnnotationPractice/src/main/java/com/example/springannotationpractice/controller/StudentController.java
@@ -4,6 +4,7 @@ import com.example.springannotationpractice.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 //@Controller
@@ -17,6 +18,13 @@ public class StudentController {
     public String getStudent(){
 
         return "Student API Working";
+    }
+
+    @PostMapping("/student")
+    public String addStudent() {
+
+        return "Student Added Successfully";
+
     }
 
     public void processStudent() {
