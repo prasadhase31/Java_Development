bf1612f00c7b322edee1424e1441aab5d8da463d
prasadhase31 <prasadhase9325@gmail.com>
2026-08-16 20:51:59 +0530

    Spring-Annotations

--- Spring/SpringAnnotationPractice/src/main/java/com/example/springannotationpractice/exception/GlobalExceptionHandler.java
+++ Spring/SpringAnnotationPractice/src/main/java/com/example/springannotationpractice/exception/GlobalExceptionHandler.java
@@ -1,4 +1,15 @@
 package com.example.springannotationpractice.exception;
 
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(StudentNotFoundException.class)
+    public String handleStudentNotFound(StudentNotFoundException ex) {
+
+        return ex.getMessage();
+    }
 }
